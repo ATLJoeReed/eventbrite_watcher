8d7651220d8b99992a47613643bc22fbfd24f7f0
Joseph Reed <atljoereed@gmail.com>
2017-09-03 21:54:17 -0400

    Wired up the watching method

--- watcher.py
+++ watcher.py
@@ -1,3 +1,6 @@
+import datetime
+import time
+
 import requests
 from twilio.rest import Client
 
@@ -14,27 +17,18 @@ class EventbriteWatcher(object):
         Args:
         __watch_time__: Amount of time to watch for the event.
         __organizer_id__: The ID of the organization to watch.
+                          NOTE: Switchyards = 6453217513
         __keyword__: Keyword to alert on.
         """
+
         self.watch_time = watch_time
         self.organizer_id = organizer_id
         self.keyword = keyword
+
         # Extract setting values...
         self.base_url = settings.BASE_URL
         self.oauth_token = settings.OAUTH_TOKEN
         self.twilio = settings.TWILIO
-
-    def send_sms(self, message):
-        client = Client(
-            self.twilio.get('account_sid', None),
-            self.twilio.get('auth_token', None),
-        )
-        response = client.messages.create(
-            to=self.twilio.get('to_number', None),
-            from_=self.twilio.get('from_number', None),
-            body=message,
-        )
-        return response
 
     def build_headers(self):
         token = "Bearer {}".format(self.oauth_token)
@@ -60,5 +54,29 @@ class EventbriteWatcher(object):
                 results.append(event_url)
         return results
 
+    def get_end_time(self):
+        return datetime.datetime.now() + \
+            datetime.timedelta(minutes=self.watch_time)
+
+    def send_sms(self, message):
+        client = Client(
+            self.twilio.get('account_sid', None),
+            self.twilio.get('auth_token', None),
+        )
+        response = client.messages.create(
+            to=self.twilio.get('to_number', None),
+            from_=self.twilio.get('from_number', None),
+            body=message,
+        )
+        return response
+
     def start_watching(self):
-        pass
+        results_found = 0
+        end_time = self.get_end_time()
+        while results_found <= 3 and datetime.datetime.now() < end_time:
+            results = self.check_eventbrite()
+            if results:
+                results_found += 1
+            for result in results:
+                self.send_sms(result)
+            time.sleep(60)
