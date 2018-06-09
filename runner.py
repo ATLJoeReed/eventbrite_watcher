8c47861f7ffbb808c4ea34300651722fbd576ecf
Joseph Reed <atljoereed@gmail.com>
2018-06-08 20:27:13 -0400

    Watch longer

--- runner.py
+++ runner.py
@@ -2,7 +2,7 @@
 from watcher import EventbriteWatcher
 
 
-watcher = EventbriteWatcher(540, '6453217513', 'made')
+watcher = EventbriteWatcher(720, '6453217513', 'made')
 # watcher = EventbriteWatcher(10, '6453217513', 'consumer')
 
 watcher.start_watching()
